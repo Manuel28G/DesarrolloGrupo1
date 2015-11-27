29eb71db15350b4f9c04d1643aed2fdc0b6fa8f1
Manuel28G <man281192@gmail.com>
2015-11-27 16:04:28 -0430

    Procedimiento añadido e insertPruebas

--- Base de datos/ScriptStoreProcedure.sql
+++ Base de datos/ScriptStoreProcedure.sql
@@ -12,7 +12,7 @@ as
 	go
 
 ------------------PROCEDURE CONSULTA ROLES DEL SISTEMA --------------------
-CREATE procedure M1_ConsultarRolesSistema
+CREATE procedure M2_ConsultarRolesSistema
 as
 	begin
 		select rol_id as id_rol, rol_nombre as nombre, rol_descripcion as descripcion
@@ -20,17 +20,27 @@ as
 	end;
 	go
 
-------------------PROCEDURE CONSULTA ROLES DE USUARIO ------------------
+------------------PROCEDURE CONSULTA ROLES DE USUARIO POR NOMBRE------------------
 CREATE procedure M1_ConsultarRolesUsuario
 	@nombre_usuario [varchar](25)
 as
 	begin
-		select roles.rol_id as id_rol, roles.rol_nombre as nombre,perol.per_rol_fecha as fecha_creacion
+		select roles.rol_id as id_rol, roles.rol_nombre as nombre,perol.per_rol_fecha as fecha_creacion,roles.rol_descripcion as descripcion
 		from ROL roles , PERSONA pers, PERSONA_ROL perol
 		where pers.per_id = perol.PERSONA_per_id AND perol.ROL_rol_id = roles.rol_id AND pers.per_nombre_usuario = @nombre_usuario
 	end;
 	go
-	
+
+--------------PROCEDURE CONSULTA ROLES DE USUARIO POR ID----------------------
+CREATE procedure M2_ConsultarRolesUsuario
+	@id_usuario [varchar](25)
+as
+	begin
+		select roles.rol_id as id_rol, roles.rol_nombre as nombre,perol.per_rol_fecha as fecha_creacion,roles.rol_descripcion as descripcion
+		from ROL roles , PERSONA pers, PERSONA_ROL perol
+		where pers.per_id = perol.PERSONA_per_id AND perol.ROL_rol_id = roles.rol_id AND pers.per_id = CONVERT(INT,@id_usuario)
+	end;
+	go
 
 ------------------PROCEDURE RESTABLECER CONTRASENA ------------------
 CREATE procedure M1_RestablecerContrasena
@@ -54,3 +64,24 @@ as
 		where ema_email= @correo_usuario and ema_principal=1
 	end;
 	go
+
+---------------PROCEDURE AGREGAR ROL--------------------------
+
+	CREATE procedure M2_AgregarRole
+	@id_usuario [varchar](25),
+	@id_rol [varchar](25)
+as
+	begin
+		insert into PERSONA_ROL values(CONVERT (date, GETDATE()),@id_usuario,@id_rol);
+	end;
+	go
+------------------PROCEDURA ELIMINAR ROL-------------------------------
+CREATE procedure M2_EliminarRole
+	@id_usuario [varchar](25),
+	@id_rol [varchar](25)
+as
+	begin
+		delete  from PERSONA_ROL  where PERSONA_per_id=@id_usuario AND ROL_rol_id=@id_rol;
+	end;
+	go
+
